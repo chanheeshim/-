d2c2d0520ace3bae33a415394e5bb6c729c0f5d5
chanheeshim <sch9247@naver.com>
2024-06-20 02:38:12 +0900

    Add files via upload

---
+++ 회색사진으로 바꾸기.java
@@ -0,0 +1,23 @@
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+public class Code13_02 {
+	static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+	public static void main(String[] args) {
+		String fileName = "c:/photo/picture01.jpg";		
+		Mat inImage = Imgcodecs.imread(fileName);
+		Mat outImage = new Mat();
+		
+		outImage = inImage.clone();
+		Imgproc.cvtColor(inImage, outImage, Imgproc.COLOR_RGB2GRAY);
+		HighGui.imshow("원본 이미지", inImage);
+		HighGui.imshow("결과 이미지", outImage);		
+		HighGui.waitKey(0);	
+		Imgcodecs.imwrite("c:/photo/picture01_out.jpg", outImage);
+		System.exit(0);
+		
+	}
+}
